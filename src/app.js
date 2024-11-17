30d3919c285a9a3b3816cadc0605dc8efb8a6a7f
Einar <einarescobarclavijo@gmail.com>
2024-11-17 23:31:19 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,34 +1,32 @@
+function generarExcusa() {
+  const who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let quien = Math.random();
+  const sujeto = Math.floor(quien * who.length);
 
+  const action = ["ate", "peed", "crushed", "broke"];
+  let accion = Math.random();
+  const accionQueHace = Math.floor(accion * action.length);
 
-function generarExcusa() {
-  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let rnd = Math.random();
-  const posicionDelWhoRandom = Math.floor(rnd*(tiposDeWho.length));
-  return tiposDeWho[posicionDelWhoRandom];
-}
-function generarExcusa() {
-  const action = ['ate', 'peed', 'crushed', 'broke'];
-  let rnd = Math.random();
-  const posicionDelActionRandom = Math.floor(rnd*(tiposDeAction.length));
-  return tiposDeAction[posicionDelActionRandom];
-}
-function generarExcusa() {
-  const what = ['my homework', 'my phone', 'the car'];
-  let rnd = Math.random();
-  const posicionDelWhatRandom = Math.floor(rnd*(tiposDeWhat.length));
-  return tiposDeWhat[posicionDelWhatRandom];
-}
-function generarExcusa() {
-  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-  let rnd = Math.random();
-  const posicionDelWhenRandom = Math.floor(rnd*(tiposDewhen.length));
-  return tiposDewhen[posicionDelWhenRandom];
-}
+  const what = ["my homework", "my phone", "the car"];
+  let que = Math.random();
+  const queHace = Math.floor(que * what.length);
 
+  const when = [
+    "before the class.",
+    "when I was sleeping.",
+    "while I was exercising.",
+    "during my lunch.",
+    "while I was praying."
+  ];
+  let cuando = Math.random();
+  const cuandoLoHace = Math.floor(cuando * when.length);
+  return (
+    who[sujeto] + action[accionQueHace] + what[queHace] + when[cuandoLoHace]
+  );
+}
 
 window.addEventListener("load", () => {
   const excusa = generarExcusa();
 
   document.body.innerHTML = `<h1>${excusa}</h1>`;
 });
-//me quede por aqui si me pudieran echar una mano con lo que sigue por que me estanque aqui
